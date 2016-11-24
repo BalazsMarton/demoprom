0f0199ccd1c1ab6841f7720805516e439786d359
BalazsMarton <balazsmarton90.joomla@gmail.com>
2016-11-24 04:02:09 +0100

    work 5 static

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
 
   get 'proba' => 'pages#proba'
 
+  get 'work5' => 'pages#work5'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
